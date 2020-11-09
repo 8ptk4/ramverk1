2863e22fe70b99bc3105a8c96a385fd306ea4478
8ptk4 <putte.karlsson@gmail.com>
2020-11-09 21:12:57 +0100

    Added: geolocation controller

---
+++ src/Controller/GeolocationController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Anax\Controllers;
+
+use Anax\Commons\ContainerInjectableInterface;
+use Anax\Commons\ContainerInjectableTrait;
+use Anax\Models\Ipstack;
+use Anax\Models\IpValidator;
+
+class GeolocationController implements ContainerInjectableInterface
+{
+
+    use ContainerInjectableTrait;
+
+
+
+    /**
+     * Request post and setup page.
+     * @method indexAction
+     * @return array
+     */
+    public function indexAction() : object
+    {
+        $title = "Geolocation";
+        $ipPost = htmlentities($this->di->get("request")->getPost("ipAddress"));
+        $ipUser = htmlentities($this->di->get("request")->getServer("REMOTE_ADDR"));
+
+        $ipStack = new IpStack($ipPost, $this->di->get("ipStack"));
+        $validator = new IpValidator($ipPost);
+
+        $page = $this->di->get("page");
+
+        $page->add("anax/geolocation/index", [
+            "title" => $title,
+            "data" => $ipStack->getInformation(),
+            "ipAddress" => $ipPost,
+            "status" => $validator->getStatus(),
+            "domain" => $validator->getDomain(),
+            "userIp" => $ipUser,
+        ]);
+
+        return $page->render([
+            "title" => $title,
+        ]);
+    }
+}
